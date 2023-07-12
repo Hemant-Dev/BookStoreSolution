40957cc653203af5d617522c7b7d615f0b380c03
Hemant-Dev <phemant092@gmail.com>
2023-07-12 20:27:34 +0530

    Add project files.

---
+++ BookStore.DataAccess/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> MyCategories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Fantasy", DisplayOrder = 4 },
+                new Category { Id = 5, Name = "Romantic", DisplayOrder = 5 }
+                );
+        }
+
+    }
+}
