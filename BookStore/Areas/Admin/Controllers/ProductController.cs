cff4146e7f34232e9e71da9d403fb08cf2b90814
Hemant-Dev <phemant092@gmail.com>
2023-07-18 11:41:58 +0530

    DataTable Working now

--- BookStore/Areas/Admin/Controllers/ProductController.cs
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -152,6 +152,26 @@ namespace BookStore.Areas.Admin.Controllers
             return Json(new { data = objFromProductList });
         }
 
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productToBeDeleted.ImageUrl.TrimStart('\\'));
+            if(System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _unitOfWork.Product.Remove(productToBeDeleted);
+            _unitOfWork.Save()
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
         #endregion
 
 
